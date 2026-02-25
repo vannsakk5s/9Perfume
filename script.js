57b401de5be57f2741a09b215aeaf917740ef97c
zakk125 <vannzakk@gmail.com>
2026-02-25 09:11:38 +0700

    v5.5

--- script.js
+++ script.js
@@ -830,7 +830,7 @@ document.addEventListener("DOMContentLoaded", () => {
 // ... (កូដផ្សេងៗនៅដដែល) ...
 
 // URL របស់ Server (ត្រូវប្រាកដថាត្រឹមត្រូវ)
-const API_BASE_URL = "https://reducing-rural-tag-jungle.trycloudflare.com";
+const API_BASE_URL = "https://finals-exceptions-colleges-associate.trycloudflare.com";
 let checkPaymentInterval = null;
 
 // ==========================================
